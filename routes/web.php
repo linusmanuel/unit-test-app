222f041fe02454608cabc4fa5def9e7df539aadd
Joao Lino <linusmannuel@gmail.com>
2024-07-07 10:04:15 +0100

    feat: add product controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', [ProductController::class, 'index'])->name('product.index');
