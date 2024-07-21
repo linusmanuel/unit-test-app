c4d4a60d65dda8f313d54ef02eac94e94868613b
Joao Lino <linusmannuel@gmail.com>
2024-07-21 17:29:15 +0100

    add product table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => $this->faker->numberBetween(1, 100),
+        ];
+    }
+}
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,35 +1,32 @@
-<div class="container">
-    <div class="row align-items-start">
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
-        </div>
-    </div>
-    <div class="row align-items-center">
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
-        </div>
-    </div>
-    <div class="row align-items-end">
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
-        </div>
-        <div class="col">
-            One of three columns
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Product Page') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                    <div class="container">
+                        @if ($products)
+                        @foreach($products as $product)
+                        <div class="row align-items-start">
+                            <div class="col">
+                                <p>Produto: {{ $product->name }}</p>
+                                <p>Preço: {{ $product->price }}</p>
+
+                            </div>
+                        </div>
+                        @endforeach
+                        @else
+                        <p>No products available.</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</x-app-layout>
